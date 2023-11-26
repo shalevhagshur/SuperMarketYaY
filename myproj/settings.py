f45a13c4148551e76f955f21534a5a24c61a8e3a
shalevhagshur <shalev.hagshur@gmail.com>
2023-11-26 23:26:55 +0200

    wsgi 2

--- myproj/settings.py
+++ myproj/settings.py
@@ -171,12 +171,3 @@ SIMPLE_JWT = {
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-# Set the DJANGO_SETTINGS_MODULE environment variable to point to your settings file
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SuperMarketYaY.settings')
-
-# Get the WSGI application from Django
-application = get_wsgi_application()
